e3527d7d4a709bf2f83c96c503d2aa2d8e3c0ee4
Joel Dag <joeldag6@gmail.com>
2025-11-23 17:42:33 +0100

    formatted file

--- DECALab1-SootUp/src/main/java/analysis/exercise2/TypeStateAnalysis.java
+++ DECALab1-SootUp/src/main/java/analysis/exercise2/TypeStateAnalysis.java
@@ -3,6 +3,7 @@ package analysis.exercise2;
 import analysis.FileStateFact;
 import analysis.ForwardAnalysis;
 import analysis.VulnerabilityReporter;
+
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -24,41 +25,41 @@ import javax.annotation.Nonnull;
 
 public class TypeStateAnalysis extends ForwardAnalysis<Set<FileStateFact>> {
 
-	public TypeStateAnalysis(@Nonnull JavaSootMethod method, @Nonnull VulnerabilityReporter reporter) {
-		super(method, reporter);
-	    // System.out.println(method.getBody());
-	}
+    public TypeStateAnalysis(@Nonnull JavaSootMethod method, @Nonnull VulnerabilityReporter reporter) {
+        super(method, reporter);
+        // System.out.println(method.getBody());
+    }
 
-	@Override
-	protected void flowThrough(@Nonnull Set<FileStateFact> in, @Nonnull Stmt stmt, @Nonnull Set<FileStateFact> out) {
-		copy(in, out);
-		// TODO: Implement your flow function here.
-		// MethodSignature currentMethodSig = method.getSignature();
-		// this.reporter.reportVulnerability(currentMethodSig, stmt);
-		prettyPrint(in, stmt, out);
+    @Override
+    protected void flowThrough(@Nonnull Set<FileStateFact> in, @Nonnull Stmt stmt, @Nonnull Set<FileStateFact> out) {
+        copy(in, out);
+        // TODO: Implement your flow function here.
+        // MethodSignature currentMethodSig = method.getSignature();
+        // this.reporter.reportVulnerability(currentMethodSig, stmt);
+        prettyPrint(in, stmt, out);
 
-	}
+    }
 
-	@Nonnull
-	@Override
-	protected Set<FileStateFact> newInitialFlow() {
-		// TODO: Implement your initialization here.
-		// The following line may be just a place holder, check for yourself if
-		// it needs some adjustments.
-		return new HashSet<>();
-	}
+    @Nonnull
+    @Override
+    protected Set<FileStateFact> newInitialFlow() {
+        // TODO: Implement your initialization here.
+        // The following line may be just a place holder, check for yourself if
+        // it needs some adjustments.
+        return new HashSet<>();
+    }
 
-	@Override
-	protected void copy(@Nonnull Set<FileStateFact> source, @Nonnull Set<FileStateFact> dest) {
-		// TODO: Implement the copy function here.
-		for (FileStateFact fsf : source) {
-			dest.add(new FileStateFact(fsf));
-		}
-	}
+    @Override
+    protected void copy(@Nonnull Set<FileStateFact> source, @Nonnull Set<FileStateFact> dest) {
+        // TODO: Implement the copy function here.
+        for (FileStateFact fsf : source) {
+            dest.add(new FileStateFact(fsf));
+        }
+    }
 
-	@Override
-	protected void merge(@Nonnull Set<FileStateFact> in1, @Nonnull Set<FileStateFact> in2, @Nonnull Set<FileStateFact> out) {
-		// TODO: Implement the merge function here.
-	}
+    @Override
+    protected void merge(@Nonnull Set<FileStateFact> in1, @Nonnull Set<FileStateFact> in2, @Nonnull Set<FileStateFact> out) {
+        // TODO: Implement the merge function here.
+    }
 
 }
